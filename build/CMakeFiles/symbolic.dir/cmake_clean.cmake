41eaf536f29ba8d9656e22730f801f691788c9d5
Gregwar <g.passault@gmail.com>
2016-05-06 16:17:32 +0200

    Symbolic

---
+++ build/CMakeFiles/symbolic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/symbolic.dir/symbolicc++.cpp.o"
+  "libsymbolic.pdb"
+  "libsymbolic.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/symbolic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
